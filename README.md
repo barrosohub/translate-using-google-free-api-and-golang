d5494e2f2ac8b39bce29c0514ed430975735865d
barrsofilho <barrosofilho.ti@gmail.com>
2021-11-13 17:40:18 -0300

    Additional Instructions on README.md and translate.go file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Translate using Google Free API and Go Lang
 (this is a simple example)
+### For test this code:
+1 - install go lang: https://golang.org/doc/install
+2 - open the folder of project in your terminal:
+cd translate-using-google-free-api-and-golang
+3 - run this command in your terminal
+go run .
--- translate.go
+++ translate.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 
-    origin_lang := "pt-br";
-    dest_lang := "en";
-    term := "laranja";       
+    origin_lang := "pt-br"; // origin language
+    dest_lang := "en"; // destination language
+    term := "laranja"; // term to translate    
 
 	url := "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&sl="+origin_lang+"&tl="+dest_lang+"&q=" + url.QueryEscape(term)
    
