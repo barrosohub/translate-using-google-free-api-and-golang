5b9c1401a72e4b6c5d0072419120e8892efb1e0e
barrsofilho <barrosofilho.ti@gmail.com>
2021-11-13 18:11:28 -0300

    format file

--- translate.go
+++ translate.go
@@ -9,19 +9,19 @@ import (
 
 func main() {
 
-    origin_lang := "pt-br"; // origin language
-    dest_lang := "en"; // destination language
-    
-	term := "laranja é uma fruta cítrica"; // term to translate    
+	term := "laranja é uma fruta cítrica" // term to translate
 
-	url := "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&sl="+origin_lang+"&tl="+dest_lang+"&q=" + url.QueryEscape(term)
-   
+	origin_lang := "pt-br" // origin language
+	dest_lang := "en"      // destination language	
+
+	url := "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&sl=" + origin_lang + "&tl=" + dest_lang + "&q=" + url.QueryEscape(term)
+
 	req, _ := http.NewRequest("GET", url, nil)
 
 	res, _ := http.DefaultClient.Do(req)
 
-    body, _ := ioutil.ReadAll(res.Body)
-    fmt.Println(string(body))
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
 
 	defer res.Body.Close()
 
